b1852eeda299d3eab1aed0da15814ba116ade151
Oussama Jilal <oussama@jilal.net>
2021-06-13 20:27:10 +0100

    Added price calculation

--- src/components/Ride.js
+++ src/components/Ride.js
@@ -1,9 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Ride.css';
 
 function Ride({ ride }) {
+    const [price, setPrice] = useState(undefined);
+
+    useEffect(() => {
+        async function fetchPrice() {
+            const result = await axios.post(
+                'http://localhost:8080/rides/calculate-price',
+                {
+                    distance: ride.distance,
+                    startTime: ride.startTime,
+                }
+            );
+            console.log(result.data);
+            setPrice(result.data.price);
+        };
+        fetchPrice();
+    }, [ride.distance, ride.startTime]);
+
     const longRideClass = ride.distance > 2 ? 'Long-ride' : ''
     return (
-        <div className={`Ride ${longRideClass}`}>Ride N°{ride.id}</div>
+        <div className={`Ride ${longRideClass}`}>
+            <div>Ride N°{ride.id}</div>
+            <div>Price: {price}</div>
+        </div>
     );
 }
 
